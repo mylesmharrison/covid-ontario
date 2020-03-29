2bf97a95ce4285af0b4be7027cb25ddae140da2f
Myles Harrison <mylesmharrison@gmail.com>
2020-03-28 21:32:37 -0400

    added methods and argparsing to scraper

--- py/MOHLTC_covid19_scrape.py
+++ py/MOHLTC_covid19_scrape.py
@@ -6,13 +6,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+
+import re
+import argparse
+
 from bs4 import BeautifulSoup
 from datetime import datetime
 import pandas as pd
-import re
 
-if __name__ == '__main__':
-    
+def fetch_html() -> str:
+    '''
+    Fetch the HTML from the MOHLTC page and save into the html/ directory
+    '''
+
+    # Get the timestamp
+    datestring = datetime.now().strftime('%Y%m%d_%H%M%S')
+    # Create the outputfilename
+    outputfilename = './html/2019-novel-coronavirus_'+datestring+'.html'
+
     # Add headless option for geckodriver
     options = Options()
     options.add_argument("--headless")
@@ -29,23 +40,40 @@ if __name__ == '__main__':
     except:
         print('Failed to get page')
 
-    # Get the timestamp
-    datestring = datetime.now().strftime('%Y%m%d_%H%M%S')
-
     # Save local copy of the HTML
-    f = open('./html/2019-novel-coronavirus_'+datestring+'.html', 'w')
+    f = open(outputfilename, 'w')
     f.write(driver.page_source)
     f.close()
 
+    # Kill the browser
+    driver.quit()
+
+    # Return the filename of the written file
+    return outputfilename
+
+def process_html_file(filepath: str, outputfilename: str = 'test.csv'):
+    '''
+    Processes the html source and saves
+    '''
+    
+    # Read in the file
+    f = open(filepath, 'r')
+    htmlsource = f.read()
+    f.close()
+
     # Create soup object
-    soup = BeautifulSoup(driver.page_source)
+    soup = BeautifulSoup(htmlsource, features="lxml")
     
     # Get the status update table
-    status_update = pd.read_html(driver.page_source)[0]
+    status_update = pd.read_html(htmlsource)[0]
 
     # Get the updated timestamp for the status updates
     updated_timestamp_p = soup.find(id='pagebody').find_all('p')[4].text.encode('ascii', errors='ignore').decode('utf8')
+    # The below returns in this format, taken from the page: March 28, 2020 at 5:30 p.m. ET
     updated_timestamp_string = re.search('.*as of(.*)', updated_timestamp_p)[1]
+    # Remove the 'ET'
+    updated_timestamp_string = updated_timestamp_string.rsplit(' ', 1)[0]
+    print(updated_timestamp_string)
     
     # Convert to ISO
     updated_iso = pd.to_datetime(updated_timestamp_string).isoformat()
@@ -59,10 +87,33 @@ if __name__ == '__main__':
     st2['updated_timestamp'] = updated_iso
 
     # Add retrieved timestamp
-    st2['retrieved_timestamp'] = pd.to_datetime(datetime.now()).isoformat()
+    st2['retrieved_timestamp'] = pd.to_datetime(datetime.now().replace(microsecond=0)).isoformat()
 
     # Write to csv
-    st2.to_csv('test.csv', header=True, index=False)
+    st2.to_csv(outputfilename, header=True, index=False)
+    
 
-    # Kill the browser
-    driver.quit()
+def process_html_directory():
+    '''
+    TODO: This function will process a whole directory of saved copies of the page and batch output csv
+    '''
+
+    pass
+
+    
+if __name__ == '__main__':
+
+    # Argument parsing
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--fromfile", help="Process saved HTML file", type=str)
+    parser.add_argument("--processdir", help="Process all saved HTML in a specified directory", type=str)
+    parser.add_argument("--nosavecsv", help="Flag to toggle saving of csv", action="store_true")
+    args = parser.parse_args()
+
+    # Get the page and save in the HTML directory
+    if(args.fromfile is None):
+        saved_html_path = fetch_html()    
+    else:
+        saved_html_path = args.fromfile
+
+    process_html_file(saved_html_path, 'testfile.csv')
